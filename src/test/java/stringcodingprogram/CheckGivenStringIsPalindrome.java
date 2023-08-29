402c7e9137f1809f99be7ae73c66a090e3b1aae7
pramukh7 <sanjaykumar.patel2134@gmail.com>
2023-08-29 13:14:39 -0400

    Added new isPalindrome program

--- src/test/java/stringcodingprogram/CheckGivenStringIsPalindrome.java
+++ src/test/java/stringcodingprogram/CheckGivenStringIsPalindrome.java
@@ -1,9 +1,31 @@
 package stringcodingprogram;
-
+/*
+ * A string is called a palindrome string if the 
+ * reverse of that string is the same as the 
+ * original string. For example, radar , level
+ */
 public class CheckGivenStringIsPalindrome {
 
 	public static void main(String[] args) {
-
+		String originalString = "radar";
+		System.out.println("Give String is Palindrome==>"+isPalindrome(originalString));
+		originalString = "OM";
+		System.out.println("Give String is Palindrome==>"+isPalindrome(originalString));
+	}
+	
+	private static boolean isPalindrome(String originalString)
+	{
+		boolean isPalindrome=false;
+		String reverseString = "";
+		for(int i=originalString.length()-1;i>=0;i--)
+		{
+			reverseString = reverseString + originalString.charAt(i);
+		}
+		if(originalString.equals(reverseString))
+		{
+			isPalindrome = true;
+		}
+		return isPalindrome;
 	}
 
 }
